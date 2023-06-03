64c466140fc8a90e935b6745aa62f0082aa56551
Anoj21 <135400708+Anoj21@users.noreply.github.com>
2023-06-03 12:12:37 +0545

    Add files via upload

---
+++ home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Design House"),
+      ),
+      body: const Center(
+        child: Text('Welcome'),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
+
+
+
+
+
+
+
+
